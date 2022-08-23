f47f816f2d6c1f4cbf7caca9d4cd1ce1b5873839
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2022-08-23 11:44:45 -0600

    Adds functionality for preserving last searched city in local storage, references value if present upon page load

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -17,14 +17,6 @@ const getCity = async (cityName) => {
 
     const response = await fetch(base + query);
     const data = await response.json();
-
+    console.log(data)
     return data[0]; 
-
 }
-
-// getCity('denver').then(data => {
-//     return getWeather(data.Key);
-// }).then(data => {
-//     console.log(data)
-// })
-// .catch(err => console.log(err))
